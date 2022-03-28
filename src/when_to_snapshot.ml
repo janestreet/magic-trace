0a9be7dff7c187e5627f051d42b7e0df5e853fa0
Clark Gaebel <leer.karats0p@icloud.com>
2022-03-28 13:09:08 -0400

    Replace `-trigger $` with `-trigger ?`
    It brings up fzf, so I think the question mark is a little clearer.
    
    Testing: echo ????hello???? ?

--- src/when_to_snapshot.ml
+++ src/when_to_snapshot.ml
@@ -19,7 +19,7 @@ let param =
        1) If you do not provide [-trigger], magic-trace takes a snapshot when either it \
        or the application under trace ends. You can Ctrl+C magic-trace to manually \
        trigger a snapshot.\n\
-       2) If you provide [-trigger $], magic-trace will open up a fuzzy-find dialog to \
+       2) If you provide [-trigger ?], magic-trace will open up a fuzzy-find dialog to \
        help you choose a symbol to trace.\n\
        3) If you provide [-trigger <FUNCTION NAME>], magic-trace will snapshot when the \
        application being traced calls the given function. This takes the fully-mangled \
@@ -28,7 +28,7 @@ let param =
        magic_trace_stop_indicator]."
   |> map ~f:(function
          | None -> Magic_trace_or_the_application_terminates
-         | Some "$" -> Application_calls_a_function Use_fzf_to_select_one
+         | Some "?" -> Application_calls_a_function Use_fzf_to_select_one
          | Some "." ->
            Application_calls_a_function (User_selected Magic_trace.Private.stop_symbol)
          | Some symbol -> Application_calls_a_function (User_selected symbol))
