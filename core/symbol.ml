dc50a6aff1c905676b2d5ea8dde9a40c942ba8b1
Clark Gaebel <leer.karats0p@icloud.com>
2022-04-20 22:08:44 -0400

    Stop using owee demangling
    Perfetto (and perf, for that matter) can demangle symbols themselves,
    they don't need us to do it. Further, this demangler was always a little
    bit incomplete -- it wouldn't tell you the line numbers of closures.

--- core/symbol.ml
+++ core/symbol.ml
@@ -17,17 +17,3 @@ let display_name = function
   | From_perf name -> name
   | From_perf_map { function_; _ } -> function_
 ;;
-
-let demangle_elf_name name =
-  if String.is_prefix name ~prefix:"caml_"
-  then name
-  else Owee_location.demangled_symbol name
-;;
-
-let demangle t =
-  match t with
-  | Unknown | Untraced | Returned | Syscall | From_perf_map _ ->
-    (* CR-someday cgaebel: Demangle java + other managed runtime names. *)
-    t
-  | From_perf name -> From_perf (demangle_elf_name name)
-;;
