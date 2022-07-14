86aebc102201192870633de13931a95b3c221401
Abena Laast <salaast@gmail.com>
2022-07-14 19:50:17 +0000

    mega traces: some early work
    Signed-off-by: Abena Laast <salaast@gmail.com>

--- src/trace.ml
+++ src/trace.ml
@@ -578,6 +578,11 @@ module Make_commands (Backend : Backend_intf.S) = struct
        and decode_opts = decode_flags
        and debug_print_perf_commands = debug_print_perf_commands
        and prog = anon ("COMMAND" %: string)
+       and _local_view =
+         flag
+           "local-view"
+           no_arg
+           ~doc:"uses local trace processor binary, for use in large trace files"
        and argv =
          flag "--" escape ~doc:"ARGS Arguments for the command. Ignored by magic-trace."
        in
@@ -764,8 +769,83 @@ module Make_commands (Backend : Backend_intf.S) = struct
            decode_opts)
   ;;
 
+  let auto_command =
+    (* mostly copied from run_command *)
+    Command.async_or_error
+      ~summary:"Does work of run command and then calls trace_processor"
+      ~readme:(fun () ->
+        "=== examples ===\n\n\
+         # Run a process, snapshotting at ^C or exit\n\
+         magic-trace run ./program -- arg1 arg2\n\n\
+         # Run and trace all threads of a process, not just the main one, snapshotting \
+         at ^C or exit\n\
+         magic-trace run -multi-thread ./program -- arg1 arg2\n\n\
+         # Run a process, tracing its entire execution (only practical for short-lived \
+         processes)\n\
+         magic-trace run -full-execution ./program\n")
+      (let%map_open.Command record_opt_fn = record_flags
+       and decode_opts = decode_flags
+       and debug_print_perf_commands = debug_print_perf_commands
+       and prog = anon ("COMMAND" %: string)
+       and _local_view =
+         flag
+           "local-view"
+           no_arg
+           ~doc:"uses local trace processor binary, for use in large trace files"
+       and argv =
+         flag "--" escape ~doc:"ARGS Arguments for the command. Ignored by magic-trace."
+       in
+       fun () ->
+         let open Deferred.Or_error.Let_syntax in
+         let%bind () = check_for_perf () in
+         let executable =
+           match Shell.which prog with
+           | Some path -> path
+           | None -> failwithf "Can't find executable for %s" prog ()
+         in
+         let%bind () =
+           record_opt_fn ~executable ~f:(fun opts ->
+               let elf = Elf.create opts.executable in
+               let%bind range_symbols =
+                 evaluate_trace_filter ~trace_filter:opts.trace_filter ~elf
+               in
+               let%bind pid =
+                 let argv = prog :: List.concat (Option.to_list argv) in
+                 run_and_record
+                   opts
+                   ~elf
+                   ~debug_print_perf_commands
+                   ~prog
+                   ~argv
+                   ~collection_mode:opts.collection_mode
+               in
+               let%bind.Deferred perf_maps = Perf_map.Table.load_by_pids [ pid ] in
+               decode_to_trace
+                 ~perf_maps
+                 ?range_symbols
+                 ~elf
+                 ~trace_scope:opts.trace_scope
+                 ~debug_print_perf_commands
+                 ~record_dir:opts.record_dir
+                 ~collection_mode:opts.collection_mode
+                 decode_opts)
+         in
+         (* let%bind.Deferred () =
+           Async_shell.run "scp" [ "MAGICTRACE:/home/abena/magic-trace/trace.fxt"; "." ]
+         in *)
+         (* let%bind.Deferred () = Async_shell.run "open" [ "http://localhost:9001" ] in
+         let%bind.Deferred () =
+           Async_shell.run "~/trace_processor" [ "-D"; "trace.fxt" ]
+         in *)
+         return ())
+  ;;
+
   let commands =
-    [ "run", run_command; "attach", attach_command; "decode", decode_command ]
+    [ "run", run_command
+    ; "attach", attach_command
+    ; "decode", decode_command
+    ; "auto", auto_command
+    ]
   ;;
 end
 
