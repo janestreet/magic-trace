42b7c90d13a9f07236ae6fcf4765ffc11c06854a
Tudor Brindus <tbrindus@janestreet.com>
2024-06-04 15:43:09 -0400

    Add an override to avoid using `--kcore`
    Signed-off-by: Tudor Brindus <tbrindus@janestreet.com>

--- src/env_vars.mli
+++ src/env_vars.mli
@@ -4,6 +4,7 @@ val debug : bool
 val experimental : bool
 val perf_path : string
 val perf_is_privileged : bool
+val perf_no_kcore : bool
 val perfetto_dir : string option
 val no_dlfilter : bool
 val fzf_demangle_symbols : bool
