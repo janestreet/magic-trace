90389562e6d6e211b31081020903a838678afa18
Kevin Svetlitski <ksvetlitski@janestreet.com>
2025-12-09 17:29:40 -0500

    Fix some bugs and add tests

--- src/trace_segment.ml
+++ src/trace_segment.ml
@@ -1,7 +1,10 @@
-[@@@disable_unused_warnings]
-
 open! Core
-module Location = Event.Location
+
+module Location = struct
+  include Event.Location
+
+  let sexp_of_t { symbol; _ } = Sexp.Atom (Symbol.display_name symbol)
+end
 
 (* Renaming this purely to avoid confusion between "callstack" and "data-structure I am
    using to maintain multiple elements" *)
@@ -12,26 +15,36 @@ module Frame = struct
     | None
     | Some of
         { location : Location.t
-        ; mutable parent : t
+        ; mutable parent : t (** [parent] may only be mutated from [None] to [Some _]. *)
         }
+  [@@deriving sexp_of]
+
+  let rec to_symbol_list acc t =
+    match t with
+    | None -> acc
+    | Some { location = { symbol; _ }; parent } ->
+      to_symbol_list (Symbol.display_name symbol :: acc) parent
+  ;;
 end
 
 module Callstack = struct
   type t =
     { time : Time_ns.Span.t
     ; mutable deepest_frame : Frame.t
+    (** [deepest_frame] may only be mutated from [None] to [Some _]. This is the same
+        invariant as on [Frame.parent], but since we can't express "pointer to a record
+        field" in OCaml, we can't easily unify these concepts. *)
     }
+  [@@deriving sexp_of]
 
-  let rec top' = function
+  let rec top = function
     | Frame.None -> Frame.None
     | Some { parent = None; _ } as frame -> frame
-    | frame -> top' frame
+    | Some { parent; _ } -> top parent
   ;;
 
-  let top t = top' t.deepest_frame
+  let top t = top t.deepest_frame
 end
-
-type t = Callstack.t Vec.t Hashtbl.M(Event.Thread).t
 
 let rec find_matching_frame (target : Symbol.t) (frame : Frame.t) : Frame.t =
   match frame with
@@ -56,12 +69,7 @@ let handle_call (stacks_at_times : Callstack.t Vec.t) time ~(src : Location.t) ~
   Vec.push stacks_at_times { time; deepest_frame }
 ;;
 
-let handle_ret
-  (stacks_at_times : Callstack.t Vec.t)
-  time
-  ~(src : Location.t)
-  ~(dst : Location.t)
-  =
+let handle_ret (stacks_at_times : Callstack.t Vec.t) time ~(dst : Location.t) =
   let matching_frame : Frame.t =
     match Vec.top stacks_at_times with
     | None -> None
@@ -79,7 +87,9 @@ let handle_ret
       Vec.iter stacks_at_times ~f:(fun callstack ->
         match Callstack.top callstack with
         | None -> callstack.deepest_frame <- frame
-        | Some top_frame -> top_frame.parent <- frame);
+        | Some top_frame as top_frame' when not (phys_equal top_frame' frame) ->
+          top_frame.parent <- frame
+        | Some _ -> ());
       frame
     | Some matching_frame -> Some { matching_frame with location = dst }
   in
@@ -90,15 +100,143 @@ let add_event' stacks_at_times (event : Event.Ok.Data.t) time =
   match event with
   | Trace { kind = Some Call; src; dst; trace_state_change = _ } ->
     handle_call stacks_at_times time ~src ~dst
-  | Trace { kind = Some Return; src; dst; trace_state_change = _ } ->
-    handle_ret stacks_at_times time ~src ~dst
+  | Trace { kind = Some Return; dst; src = _; trace_state_change = _ } ->
+    handle_ret stacks_at_times time ~dst
   | Trace _
   | Event.Ok.Data.Power _
   | Event.Ok.Data.Stacktrace_sample _
   | Event.Ok.Data.Event_sample _ -> failwith "Unimplemented"
 ;;
 
-let add_event t (event : Event.Ok.t) =
+type t = Callstack.t Vec.t Hashtbl.M(Event.Thread).t
+
+let _add_event (t : t) (event : Event.Ok.t) =
   let stacks_at_times = Hashtbl.find_or_add t event.thread ~default:Vec.create in
   add_event' stacks_at_times event.data event.time
+;;
+
+let%test_module _ =
+  (module struct
+    let setup_test () =
+      let callstacks = Vec.create () in
+      let ip = ref (-1) in
+      let time = ref Time_ns.Span.zero in
+      let incr_time () = time := Time_ns.Span.(!time + of_int_ns 1) in
+      let location (name : string) : Location.t =
+        incr ip;
+        Location.
+          { instruction_pointer = Int64.of_int !ip
+          ; symbol_offset = 0
+          ; symbol = From_perf name
+          }
+      in
+      let internal_stack = Vec.of_list [ location "main" ] in
+      let call (name : string) =
+        incr_time ();
+        let new_location = location name in
+        let event =
+          Event.Ok.Data.Trace
+            { kind = Some Call
+            ; src = Stack.top_exn internal_stack
+            ; dst = new_location
+            ; trace_state_change = None
+            }
+        in
+        Stack.push internal_stack new_location;
+        add_event' callstacks event !time
+      in
+      let return ?dst () =
+        incr_time ();
+        let src = Stack.pop_exn internal_stack in
+        let event =
+          Event.Ok.Data.Trace
+            { kind = Some Return
+            ; src
+            ; dst =
+                (match dst with
+                 | Some dst -> location dst
+                 | None -> Stack.top_exn internal_stack)
+            ; trace_state_change = None
+            }
+        in
+        add_event' callstacks event !time
+      in
+      callstacks, call, return
+    ;;
+
+    let join_strings (lists : string list list) : string =
+      let max_len =
+        List.fold lists ~init:0 ~f:(fun acc lst -> Int.max acc (List.length lst))
+      in
+      let width = 20 in
+      List.init max_len ~f:(fun row_idx ->
+        List.map lists ~f:(fun lst ->
+          let s = List.nth lst row_idx |> Option.value ~default:"" in
+          sprintf "%-*s" width s)
+        |> String.concat)
+      |> String.concat ~sep:"\n"
+    ;;
+
+    let print_callstacks (callstacks : Callstack.t Vec.t) =
+      Vec.to_list callstacks
+      |> List.map ~f:(fun callstack -> Frame.to_symbol_list [] callstack.deepest_frame)
+      |> List.rev
+      |> join_strings
+      |> print_endline
+    ;;
+
+    (*= 
+       Assume no tail-call-optimization is performed: 
+
+       let fn2 () = () 
+       let fn3 () = ()
+
+       let fn1 () = 
+         fn2 () 
+         fn3 () 
+       ;;
+
+       let main () = fn1 ()
+    *)
+    let%expect_test "Sanity-check [add_event']" =
+      let callstacks, call, return = setup_test () in
+      call "fn1";
+      call "fn2";
+      return ();
+      call "fn3";
+      return ();
+      return ();
+      print_callstacks callstacks;
+      [%expect {||}]
+    ;;
+
+    (*= 
+       Assume no tail-call-optimization is performed, and we started tracing during the execution of [main],
+       so we never saw the call to [start]:
+
+       let fn2 () = () 
+       let fn3 () = ()
+
+       let fn1 () = 
+         fn2 () 
+         fn3 () 
+       ;;
+
+       let main () = fn1 ()
+
+       let start () = main ()
+    *)
+    let%expect_test "A return to a function we never saw the call for" =
+      let callstacks, call, return = setup_test () in
+      call "fn1";
+      call "fn2";
+      return ();
+      call "fn3";
+      return ();
+      return ();
+      return ~dst:"start" ();
+      print_callstacks callstacks;
+      [%expect {||}]
+    ;;
+  end)
 ;;
