309595033b0b4e053efe36301377883239025ba5
Kevin Svetlitski <ksvetlitski@janestreet.com>
2025-12-12 15:28:56 -0500

    Delete the old trace-segment code

--- src/trace_segment.mli
+++
@@ -1,8 +0,0 @@
-open! Core
-
-(** A continuous, lossless, error-free segment of a trace corresponding to a single
-    thread. *)
-type t
-
-val create : unit -> t
-val add_event : t -> Event.Ok.Data.t -> Timestamp.t -> unit
